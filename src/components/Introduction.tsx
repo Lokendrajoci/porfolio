60c7a3e895aa69a445cff036ed0d1476264512cb
Lokendrajoci <lokendrajoci@gmail.com>
2025-01-08 18:15:35 +0545

    changes

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,29 +1,24 @@
 "use client";
+
 import { TypewriterEffectSmooth } from "../components/ui/typewriter-effect";
 import React from "react";
 
 export default function Introduction() {
   const words = [
-    {
-      text: "a",
-    },
-    {
-      text: "passionate",
-    },
-    {
-      text: "developer .",
-    },
+    { text: "a" },
+    { text: "passionate" },
+    { text: "developer." }, 
   ];
+
   return (
-    <>
-      <div className="w-full grid grid-cols-4 md:grid-cols-7 mt-5">
-        <div className="col-span-4 md:col-span-2 md:col-start-2 md:col-end-5">
-          <h1 className="text-white text-xl sm:text-lg md:text-2xl">
-            Hi, I'm Lokendra Joshi
-          </h1>
-          <TypewriterEffectSmooth words={words} />
-        </div>
+    <div className="w-full grid grid-cols-4 md:grid-cols-7 mt-5">
+      <div className="col-span-4 md:col-span-2 md:col-start-2 md:col-end-5 ">
+ 
+        <h1 className="text-white text-lg md:text-2xl font-bold">
+          Hi, I&rsquo;m Lokendra Joshi 
+        </h1>
+        <TypewriterEffectSmooth words={words} />
       </div>
-    </>
+    </div>
   );
 }
